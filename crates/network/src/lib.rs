71e937de500b404bed6eb1cee2b76289363216e9
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-10-16 14:26:24 +0300

    simplify test msg

--- crates/network/src/lib.rs
+++ crates/network/src/lib.rs
@@ -100,7 +100,7 @@ impl Network {
         loop {
             select! {
                 _ = interval.tick() => {
-                    let test_message = format!("peer {} test {}", swarm.local_peer_id(), msg_count);
+                    let test_message = format!("test {}", msg_count);
                     if let Err(e) = swarm
                         .behaviour_mut()
                         .gossipsub
