dc345a3134d8db9d1bc0bbd559438f846f9dc12f
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2026-01-15 18:44:24 +0300

    network rpc impl

--- bin/citrea/src/main.rs
+++ bin/citrea/src/main.rs
@@ -238,6 +238,7 @@ where
         rollup_config.rpc.clone(),
     )?;
 
+    let network_globals = Arc::new(citrea_network::NetworkGlobals::new());
     if matches!(node_type, NodeWithConfig::LightClientProver(_)) {
         register_healthcheck_rpc_light_client_prover(&mut rpc_module, da_service.clone())
             .expect("Failed to register healthcheck RPC for light client prover");
@@ -252,6 +253,7 @@ where
             &mut rpc_module,
             sequencer_client_url,
             l2_block_rx,
+            network_globals.clone(),
         )?;
     }
 
@@ -271,6 +273,7 @@ where
                     rpc_module,
                     backup_manager,
                     task_executor.clone(),
+                    network_globals,
                 )
                 .expect("Could not start sequencer");
 
@@ -370,6 +373,7 @@ where
                     l2_block_tx,
                     rpc_module,
                     backup_manager,
+                    network_globals
                 )
                 .await
                 .expect("Could not start full-node");
